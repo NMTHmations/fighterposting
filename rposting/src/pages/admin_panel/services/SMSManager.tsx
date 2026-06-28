d0571413cb683691eaf53278fc5a872b1ac0fa8c
NMTHmations <125398293+NMTHmations@users.noreply.github.com>
2026-06-29 00:20:55 +0200

    Propagandist addition backend almost done

--- rposting/src/pages/admin_panel/services/SMSManager.tsx
+++ rposting/src/pages/admin_panel/services/SMSManager.tsx
@@ -1,12 +1,54 @@
 import { createEffect, createSignal } from "solid-js";
 import SMSBar from "../../../components/SMSBar";
 import ModalWindow from "../../../components/ModalWindow";
+import { Plus, User, ImageIcon, XIcon } from "lucide-solid";
 
 export default function SMSManager() {
 
     const [SMSList, setSMSList] = createSignal<Array<{id: number, date: string, message: string}>>([]);
     const [isRefetch, setIsRefetch] = createSignal(true);
     const [showWindow, setShowWindow] = createSignal(false);
+    const [Propagandist, setPropagandist] = createSignal(false);
+    const [getFile, setFile] = createSignal<File | null>(null);
+    const [imageSrc, setImageSrc] = createSignal<string | null>(null);
+    const [uploaded, setUploaded] = createSignal(false);
+
+    const handleFileChange = (event: Event) => {
+        const target = event.target as HTMLInputElement;
+        if (target.files && target.files.length > 0) {
+            const file = target.files[0];
+            if (["image/png","image/jpeg","image/jpg","image/gif"].includes(file.type) && file.size <= 5 * 1024 * 1024) {
+                setFile(file);
+                const reader = new FileReader();
+                reader.onload = () => {
+                    setImageSrc(reader.result as string);
+                };
+                reader.readAsDataURL(file);
+                setUploaded(true);
+            }
+            else {
+                if (file.size > 5 * 1024 * 1024) {
+                    alert("A feltöltött fájl mérete túl nagy! Maximum 5MB lehet.");
+                }
+                else if (!["image/png","image/jpeg","image/jpg","image/gif"].includes(file.type)) {
+                    alert("Csak az alábbi típusú képfájlokat tölthetsz fel: PNG, JPEG/JPG, GIF.");
+                }
+                setFile(null);
+                setImageSrc(null);
+                setUploaded(false);
+            }
+        }
+        else{
+            setFile(null);
+            setUploaded(false);
+        }
+    };
+
+    const removeUpload = () => {
+        setFile(null);
+        setImageSrc(null)
+        setUploaded(false);
+    }
 
     const getAllSMS = async () => {
         const response = await fetch(import.meta.env.VITE_API_URL + `fighter/sms/`, {
@@ -76,6 +118,7 @@ export default function SMSManager() {
     return (
         <div>
             <h1 class="text-2xl font-bold mb-4">SMS Kezelő</h1>
+            <button class="bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-800 mt-4" onClick={() => setPropagandist(true)}><Plus/></button>
             {SMSList().length > 0 ?
             SMSList().map((sms) => (
                 <SMSBar id={sms.id} date={sms.date} message={sms.message} setIsRefetch={setIsRefetch}/>
@@ -106,6 +149,42 @@ export default function SMSManager() {
             }}>Hozzáadás</button>
         </ModalWindow>
         : null }
+        {
+            Propagandist() ?
+            <ModalWindow setOpenAddWindow={setPropagandist}>
+                <h1 class="text-xl font-bold mb-4">Propagandista hozzáadása</h1>
+                <div class="flex flex-row">
+                <div class="w-1/2 flex flex-col justify-center items-center">
+                    { uploaded() === true ?
+                    <img src={imageSrc()!} class="w-[100%] p-2"></img>
+                    :
+                    <User class="w-32 h-32 text-gray-600" />
+                    }
+                </div>
+                <div class="w-1/2">
+                    <p>Név:</p>
+                    <input type="text" id="propagandistName" class="border border-gray-300 rounded-md p-2 w-full mb-4" placeholder="Propagandista neve" />
+                    <p class="mb-4">Propagandista fotója:</p>
+                    { uploaded() == false ? 
+                    <>
+                    <input type="file" id="file-upload" class="hidden" onChange={handleFileChange}/>
+                    <label for="file-upload" class="inline-block mb-2 w-full rounded bg-[#ff6004] shadow-md text-white text-center p-2 hover:shadow-sm hover:bg-[#df5200]">Kép feltöltése</label>
+                    </>
+                    :
+                    <div class="flex flex-row">
+                    <p><ImageIcon class="inline-block pr-2 pb-1"/>{getFile()?.name}</p>
+                    <button class="ml-2 rounded w-6 hover:bg-gray-300" onclick={removeUpload}><XIcon/></button>
+                    </div>
+                    }
+                </div>
+                </div>
+                <div class="w-full flex justify-start">
+                    <button class="mb-2 rounded bg-[#ff6004] shadow-md text-white text-center p-2 hover:shadow-sm hover:bg-[#df5200]">Mentés</button>
+                </div>
+            </ModalWindow>
+            :
+            null
+        }
         </div>
     );
 }
